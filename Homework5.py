2b1d3a61185115951c6eb9c5d11f244e8f116a0e
AlexanerK <146099678+AlexanerK@users.noreply.github.com>
2024-04-18 22:15:28 +0400

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,13 @@
+my_list = ["apple","banana","citruse","fignja","eshe_odna_fignja"]
+print(my_list)
+print(my_list[0],my_list[-1])
+print(my_list[2:5])
+my_list[2] = "amino"
+print(my_list)
+my_dict = {'apple' : 'яблоко', 'banana' : 'банан'}
+print(my_dict)
+print(my_dict['apple'])
+my_dict ['apple']  = "апельсин"
+my_dict.update({'dog' : 'собака'})
+print(my_dict)
+
