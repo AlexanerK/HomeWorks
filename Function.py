3a45bd6f1de26a329f7c77aee22ee055d2da2c13
AlexanerK <146099678+AlexanerK@users.noreply.github.com>
2024-04-29 23:03:35 +0400

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,8 @@
+list_ = ["BMW", "MB", "Lada", "KIA", "HONDA"]
+cars_count = 0
+for i in list_:
+        print(f"Я езжу на автомобиле марки", i)
+        cars_count += 10
+        print(cars_count)
+
+
